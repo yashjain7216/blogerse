cc9d6d5e8fa4c0a0f533964058a2bc57f76dd1f7
yashjain7216 <76202997+yashjain7216@users.noreply.github.com>
2024-08-29 15:10:31 +0530

    Update AdminController.java

--- src/main/java/com/project/blogerse/controller/AdminController.java
+++ src/main/java/com/project/blogerse/controller/AdminController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500"})
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "https://blogerse-frontend.vercel.app/"})
 @RequestMapping("/admin")
 public class AdminController {
 
