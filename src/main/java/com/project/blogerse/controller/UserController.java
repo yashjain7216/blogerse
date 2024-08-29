cc8424c650c4ddd10ab1e9f9f4f20f9dfcb3f7f3
yashjain7216 <76202997+yashjain7216@users.noreply.github.com>
2024-08-29 18:57:34 +0530

    Update UserController.java

--- src/main/java/com/project/blogerse/controller/UserController.java
+++ src/main/java/com/project/blogerse/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = {"https://blogerse-frontend.vercel.app"})
+
 @RequestMapping("/user")
 public class UserController {
 
