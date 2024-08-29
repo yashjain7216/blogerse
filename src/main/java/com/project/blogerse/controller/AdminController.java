de4308d7da855012c5b333ea7e57224b7e126967
yashjain7216 <76202997+yashjain7216@users.noreply.github.com>
2024-08-29 18:56:47 +0530

    Update AdminController.java

--- src/main/java/com/project/blogerse/controller/AdminController.java
+++ src/main/java/com/project/blogerse/controller/AdminController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@CrossOrigin(origins = {"https://blogerse-frontend.vercel.app"})
+
 @RequestMapping("/admin")
 public class AdminController {
 
